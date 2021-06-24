7e9780f3fa565b59312081dd9ceb3075b5017bd5
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-24 14:11:04 +0900

    행렬의 덧셈

---
+++ src/Exercise/행렬의 덧셈.js
@@ -0,0 +1,12 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12950
+
+function solution(arr1, arr2) {
+  let answer = [];
+  for (let i = 0; i < arr1.length; i++) {
+    answer[i] = [];
+    for (let j = 0; j < arr1[i].length; j++) {
+      answer[i][j] = arr1[i][j] + arr2[i][j];
+    }
+  }
+  return answer;
+}
